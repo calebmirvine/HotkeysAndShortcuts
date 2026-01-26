c3cf0103104e74800b2f4278d7c19560d8edc704
Caleb Irvine <irvinecaleb1@gmail.com>
2026-01-25 16:36:27 -0800

    Initial Commit

---
+++ HotkeysAndShortcuts/HotkeysAndShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotkeysAndShortcutsApp.swift
+//  HotkeysAndShortcuts
+//
+//  Created by Caleb on 2026-01-25.
+//
+
+import SwiftUI
+
+@main
+struct HotkeysAndShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
